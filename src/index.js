6427c048cee5c51b612714c681324935b59b7731
AkinoK <akashima@my.centennialcollege.ca>
2024-01-05 14:38:04 -0500

    Edit index.js adding router

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createStore from './reducks/store/store';
+import {ConnectedRouter} from "connected-react-router";
+import * as History from "history";
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-
-export const store = createStore(); //作ったstoreを実行
+const history = History.createBrowserHistory;
+export const store = createStore(history); //作ったstoreを実行
 
 //propsにstoreを渡すと、ラップしたコンポーネントにstoreも情報を渡す
 //react-reduxのconnect関数を使えるようにする⇒ReactとReduxを接続してstoreを変更できるように
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history = {history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
