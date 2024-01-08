838181a7bb53a50a08fcfd5de2ccfe704bf623a0
AkinoK <akashima@my.centennialcollege.ca>
2024-01-08 15:31:33 -0500

    Create firebase folder

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCqAYk2WEa9i4yzfowhAP0Wx9wyxujMA6A",
+    authDomain: "ec-app-a7b7e.firebaseapp.com",
+    projectId: "ec-app-a7b7e",
+    storageBucket: "ec-app-a7b7e.appspot.com",
+    messagingSenderId: "232689798964",
+    appId: "1:232689798964:web:f7a58e60509bbda9595f61",
+    measurementId: "G-34924WFJW9"
+  };
