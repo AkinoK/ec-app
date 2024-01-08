ae8e248acde3d8f1865054d0cf6f4e18494766dc
AkinoK <akashima@my.centennialcollege.ca>
2024-01-08 17:08:52 -0500

    Create SignUp component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router';
-import {Login, Home} from './templates';
+import {Login, Home, SignUp} from './templates';
 
 const Router = () => {
     return (
         // method Switch & exact 部分一致ではなく完全一致
          // /がなくてもホームを表示
         <Switch>
+            <Route exact path ={"/signup" } component = {SignUp} />
             <Route exact path ={"/login" } component = {Login} />
             <Route exact path ={"/(/)?"}  component = {Home} />
            
--- src/templates/index.js
+++ src/templates/index.js
@@ -1,3 +1,4 @@
 // entry pointとなるテンプレート
 export {default as Home} from './Home';
 export {default as Login} from './Login';
+export {default as SignUp} from './SignUp';
